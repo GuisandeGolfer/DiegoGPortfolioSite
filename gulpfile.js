42e0b5d3edee4f7cd4e59d2ff8b79c4a7785d870
DGuisande873@gmail.com <DGuisande873@gmail.com>
2019-06-19 21:59:50 -0700

    edit of style.css identation and add const and arrow functions to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
-var gulp = require('gulp');
-var plumber = require('gulp-plumber');
-var uglify = require('gulp-uglify');
-var sass = require('gulp-sass');
-var wait = require('gulp-wait');
-var rename = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer');
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const wait = require('gulp-wait');
+const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('scripts', function() {
+gulp.task('scripts', () => {
     return gulp.src('js/scripts.js')
         .pipe(plumber(plumber({
-            errorHandler: function (err) {
+            errorHandler: err => {
                 console.log(err);
                 this.emit('end');
             }
@@ -19,18 +19,22 @@ gulp.task('scripts', function() {
                 comments: '/^!/'
             }
         }))
-        .pipe(rename({extname: '.min.js'}))
+        .pipe(rename({
+            extname: '.min.js'
+        }))
         .pipe(gulp.dest('js'));
 });
 
-gulp.task('styles', function () {
+gulp.task('styles', () => {
     return gulp.src('./scss/styles.scss')
         .pipe(wait(250))
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
         .pipe(gulp.dest('./css'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', () => {
     gulp.watch('js/scripts.js', gulp.series('scripts'));
     gulp.watch('scss/styles.scss', gulp.series('styles'));
 });
